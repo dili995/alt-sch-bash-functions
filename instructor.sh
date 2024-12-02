4749abc77a7d4c832d20b74e6e49c16313dea04f
Kamdilichukwu Egenti <egentikizito@gmail.com>
2024-12-03 00:44:38 +0100

    Added my information_bio

--- instructor.sh
+++ instructor.sh
@@ -1,12 +1,21 @@
-who_am_i() {
-  echo "Hello, I am the Cloud Instructo🎉"
-}
+#!/bin/bash
 
-# write bash fucntions that prints out the following infomation about you
+fullName="Kamdilichukwu Kizito Egenti"
+age=29
+Goals=("- to learn the concepts and basics of cloud engineering""\n- practice as much as I can""\n- be able to build and deploy real life projects""\n- get a SRE, SysAdmin, Cloud engr or Devops job in 2025")
 
-# Fullname
-# Bio
-# Year in tech
-# Dream job
-# Goals ⚽
 
+echo "Hi stranger! What's your name?"
+
+read your_name
+
+echo -e "\n\nHello $your_name, my name is $fullName.
+    \nI am $age yrs old and my country of origin is Nigeria.
+    \nThis is my first year trying out tech and I decided to 
+    \nstudy cloud engineering so that i would be a Devops 
+    \nengineer in the future. I like the field because it deals 
+    \nwith automation, which helps to make processes easier and 
+    \nbasically everything is done on the cloud these days. My 
+    \ndream job would be working in a game dev company as a 
+    \ncloud or devops engineer. My current goals are: 
+    \n$Goals\n\n"
